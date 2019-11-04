fe804559d9482911a3a366104419ecd2b7b25b0e
ness6514 <ness6514@student.cptc.edu>
2019-11-04 14:46:34 -0800

    Add project files.

---
+++ TriviaAPIConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TriviaAPIConsumer
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            TriviaClient trivia = new TriviaClient();
+            string result = await trivia.GetTriviaQuestionsAsync();
+
+            Console.WriteLine("Received trivia questions.\n\n");
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
